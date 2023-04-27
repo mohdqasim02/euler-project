32eecade8ede4e4d0098a7d40b184bb636c1cb2a
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-04-27 10:33:52 +0530

    Add feature to add sum of multiples of a number

--- src/euler-project.js
+++ src/euler-project.js
@@ -1,3 +1,11 @@
-const sumOfMultiples = function(numbers, ) {
+const sumOfMultiples = function(number, upto) {
+  let sum = 0;
 
+  for(let index = 0; index < upto; index += number) {
+    sum += number;
+  }
+
+  return sum;
 };
+
+exports.sumOfMultiples = sumOfMultiples;
--- tests/euler-project-test.js
+++ tests/euler-project-test.js
@@ -1,35 +1,26 @@
 const {title, display} = require("../lib/format.js");
 const {assert, summary} = require("../lib/test-framework.js");
+const {sumOfMultiples} = require("../src/euler-project.js");
 
 const it = function(testName, funcName, testData) {
   assert(testName, funcName, testData);
 };
 
-const testAssert = function() {
-  it("should pass if elements are equal", "areEqual", {
-    expected: 1, 
-    actual: 1 
+const testSumOfMultiples = function() {
+  it("should give zero for sum upto zero elements", "sumOfMultiples", {
+    expected: 0,
+    actual: sumOfMultiples(3, 0)
   });
 
-  it("should pass when two list are equal", "areEqual", {
-    expected: [1, [2], '3', {a:2}], 
-    actual: [1, [2], '3', {a:2}]
+  it("should give the number for sum of one element", "sumOfMultiples", {
+    expected: 3,
+    actual: sumOfMultiples(3, 1)
   });
 
-  it("should pass when two sets are equal", "areEqual", {
-    expected: {1:1, 2:2, 3:3}, 
-    actual: {1:1, 2:2, 3:3}
-  });
-
-  it("should pass with different data types and nesting", "areEqual", {
-    expected: {1:1, 2:[1, 2, 3], 3:{a:1, b:2, c:[4, 5]}}, 
-    actual: {1:1, 2:[1, 2, 3], 3:{a:1, b:2, c:[4, 5]}}
-  });
-}
-
+};
 const test = function() {
   display(title("Testing test-framework.js"));
-  testAssert();
+  testSumOfMultiples();
   summary();
 }
 
